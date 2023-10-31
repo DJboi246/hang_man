d5960f13bda4075b88317018497d5cc2ddf5733f
DJboi246 <subhasdheeraj@gmail.com>
2023-10-31 20:08:55 +0100

    disposeflameaudio

--- lib/main.dart
+++ lib/main.dart
@@ -288,6 +288,13 @@ class _HangmanState extends State<Hangman> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    FlameAudio.bgm.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
